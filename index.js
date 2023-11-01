9c88d5fd9cc3aaf17d833b31f60b666d115635b4
imanebk-github <imane.elbakir@gmail.com>
2023-11-01 18:39:19 +0100

    customized html templates

--- index.js
+++ index.js
@@ -3,33 +3,6 @@ const http = require("http");
 const url = require("url");
 
 //////////////////////////////////
-//////files
-
-// blocking synchronous way
-// const textInput = fs.readFileSync("./txt/input.txt", "utf-8");
-// console.log(textInput);
-
-// const textOutput = `this is what we know about the avocado: ${textInput}.\nCreated on ${Date.now()}`;
-
-// fs.writeFileSync("./txt/output.txt", textOutput);
-// console.log("File written!");
-
-// Non-blocking, asynchronous way
-// fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
-//   if (err) return console.error(Error 💥💥);
-//   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
-//     console.log(data2);
-//     fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
-//       console.log(data3);
-//       fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
-//         console.log("Your file has been written successfully.👍");
-//       });
-//     });
-//   });
-// });
-// console.log("I will read this file!");
-
-//////////////////////////////////
 //////server
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
