d0a6874344893be85b99590ba23c679a19cb235c
imanebk-github <imane.elbakir@gmail.com>
2023-11-01 14:50:12 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,59 @@
+const fs = require("fs");
+const http = require("http");
+const url = require("url");
+
+//////////////////////////////////
+//////files
+
+// blocking synchronous way
+// const textInput = fs.readFileSync("./txt/input.txt", "utf-8");
+// console.log(textInput);
+
+// const textOutput = `this is what we know about the avocado: ${textInput}.\nCreated on ${Date.now()}`;
+
+// fs.writeFileSync("./txt/output.txt", textOutput);
+// console.log("File written!");
+
+// Non-blocking, asynchronous way
+// fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+//   if (err) return console.error(Error 💥💥);
+//   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+//     console.log(data2);
+//     fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+//       console.log(data3);
+//       fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
+//         console.log("Your file has been written successfully.👍");
+//       });
+//     });
+//   });
+// });
+// console.log("I will read this file!");
+
+//////////////////////////////////
+//////server
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
+const server = http.createServer((req, res) => {
+  console.log(req.url);
+  const pathName = req.url;
+  if (pathName === "/" || pathName === "/overview") {
+    res.end("This is the OVERVIEW");
+  } else if (pathName === "/product") {
+    res.end("This is the PRODUCT");
+  } else if (pathName === "/api") {
+    res.writeHead(200, {
+      "content-type": "application/json",
+    });
+    res.end(data);
+  } else {
+    res.writeHead(404, {
+      "content-type": "text/html",
+      "my-own-header": "hello-world",
+    });
+    res.end("<h1>page not found</h1>");
+  }
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("listening to server on port 8000");
+});
